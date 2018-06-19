80307b79c27518fae2b3099f2a87093c1121ed19
Guido Chari <charig@gmail.com>
2018-06-19 14:59:22 +0200

    Bootloading experiment finished

--- Bootloading/runBenchs.sh
+++ Bootloading/runBenchs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 SCRIPT_PATH=`dirname $0`
 
-#scripts/runExperimentInVBox.sh "Ubuntu"
+RESULTS_FILE_NAME="boottime.data"
+scripts/runExperimentInVBox.sh "Ubuntu"
 scripts/runExperimentInVBox.sh "CogNOS-iso-debug"
+
+rm $RESULTS_FILE_NAME
+cat *.data >> $RESULTS_FILE_NAME
+mv $RESULTS_FILE_NAME $RESULTS_FILE_NAME.bak
+rm *.data
+mv $RESULTS_FILE_NAME.bak $RESULTS_FILE_NAME.data 
