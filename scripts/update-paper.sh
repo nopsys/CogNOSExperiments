9ee79846d42d8d2adb076871631c7482391169de
Guido Chari <charig@gmail.com>
2018-07-03 09:48:06 +0200

    Change image path when moving to paper

--- scripts/update-paper.sh
+++ scripts/update-paper.sh
@@ -9,5 +9,7 @@ REPORT_DIR="$BASE/Report"
 
 INFO "Copying files to paper dir"
 cp "$REPORT_DIR/images/"*.pdf "$PAPER_DIR/Imagenes/"
+
+sed -i.bak 's/\/Users\/guidochari\/Documents\/Projects\/Research\/Nopsys\/CogNOSExperiments\/Report\/images/Imagenes/g' $REPORT_DIR/report.tex
 cp "$REPORT_DIR/report.tex" "$PAPER_DIR/experiments.tex"
 OK "Done"
